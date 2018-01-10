d00d406b6ac927e5d65d15f89e3d6a8f5826c2f5
Sebastian Grosu <sgrosu@overgrounddata.com>
2018-01-10 09:44:25 +0000

    10/01

---
+++ Variables/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := "golang"
+	c := 4.17
+	d := true
+	var g float64
+
+	fmt.Printf("%v \t %T \n",a,a)
+	fmt.Printf("%v \t %T\n",b,b)
+	fmt.Printf("%v \t %T\n",c,c)
+	fmt.Printf("%v \t %T \n",d,d)
+	fmt.Printf("%v \t %T \n",g,g)
+
+}
---
+++ shadowing/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func max(x int) int{
+	return 42 + x
+}
+
+func main(){
+	max := max(7)
+	fmt.Println(max)
+	x := max(4)
+	fmt.Println(x)
+}
